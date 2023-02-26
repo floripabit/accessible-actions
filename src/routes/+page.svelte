dae064784259032205b37e94dff248760a2520e7
Adriano Pires <adriano.pires@floripabit.com.br>
2023-02-26 01:11:16 -0300

    Simple speech recognition

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,58 @@
 <script lang="ts">
     import type { ActionData } from './$types';
+    import { createEventDispatcher, onMount } from 'svelte';
     
     let prompt = "";
     export let form: ActionData;
+    const dispatch = createEventDispatcher();
+
+    let recognizing = false;
+    let recognition = null;
+;
+
+    function startRecognition() {
+        console.log('startinggg');
+        if (!recognizing) {
+        //recognition = new webkitwebkitSpeechRecognition();
+        const SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+        recognition = new SpeechRecognition();
+        recognition.lang = 'pt-BR';
+        recognition.continuous = true;
+        recognition.onresult = (event) => {
+            const transcript = event.results[0][0].transcript;
+            console.log('oiiii Result');
+            console.log(transcript);
+            dispatch('recognized', { transcript });
+        };
+        recognition.onend = () => {
+            console.log('on End');
+            recognizing = false;
+        };
+        recognition.onerror = (event) => {
+            console.error(event.error);
+            console.error(event);
+        };
+        recognition.start();
+        console.log('oiiii');
+        recognizing = true;
+        }
+    }
+
+    function stopRecognition() {
+        console.log('parei!');
+        if (recognizing) {
+            recognition.stop();
+            recognizing = false;
+        }
+    }
+
+    onMount(() => {
+        if (!('webkitSpeechRecognition' in window)) {
+        console.warn('Speech recognition not available');
+        }
+        const SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+        recognition = new SpeechRecognition();
+    });
 </script>
   
 <form method="POST" action="?/getChatGPTAnswer">
@@ -14,3 +64,12 @@
 {#if form?.generatedText}
     <p>{form?.generatedText}</p>
 {/if}
+
+
+<button on:click={startRecognition}>Start</button>
+<button on:click={stopRecognition}>Stop</button>
+
+
+
+
+
